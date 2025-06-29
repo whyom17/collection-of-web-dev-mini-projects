d20e1aa65ec7c72d1e619d6e256cc1b7c21da833
whyom17 <whyom17@gmail.com>
2025-06-29 17:24:42 +0530

    Game Completed

--- bubble-game/script.js
+++ bubble-game/script.js
@@ -1,3 +1,32 @@
+let score = 0;
+document.querySelector("#scoreval").innerHTML= score;
+
+let timer = 60;
+document.querySelector("#timerval").innerHTML=timer;
+
+let hit;
+
+nextHit();
+setTimer();
+createBubble();
+ 
+function nextHit(){
+    hit = Math.floor(Math.random()*10);
+    document.querySelector("#hitval").innerHTML= hit;
+}
+
+function setTimer(){
+    if (timer>=0){
+        document.querySelector("#timerval").innerHTML=timer;
+        timer-=1;
+        setTimeout(setTimer,1000);
+    }
+    else{
+        // clearTimeout(timer)
+        document.querySelector("#bbtm").innerHTML=`<h1>GAME OVER<br>YOUR FINAL SCORE: ${score}<h1>`;
+    }
+}
+
 function createBubble(){
     var clutter= "";
 
@@ -7,6 +36,19 @@ function createBubble(){
     }
 
     document.querySelector("#bbtm").innerHTML= clutter;
+
+    let bubble= document.querySelectorAll(".bubble")
+    bubble.forEach(function (div) {
+    div.addEventListener("click", function () {
+        if (div.textContent == hit) {
+        score += 10;
+        document.querySelector("#scoreval").innerHTML = score;
+        nextHit();
+        createBubble();
+        }
+    });
+    });
 }
 
-createBubble();
+
+//  also try by using the concept of 'Event Bubbling'
